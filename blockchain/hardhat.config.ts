e0f308e585f139faa1ed28dcb3d00d55c308f61c
Ronfflex <aiglevisnky@gmail.com>
2023-08-12 01:59:43 +0200

    init

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { env } from "process";
+
+const config: HardhatUserConfig = {
+  // defaultNetwork: "sepolia",
+  // networks: {
+  //   hardhat: {},
+  //   sepolia: {
+  //     url: env.ETH_SEPOLIA_URL,
+  //   },
+  // },
+  solidity: "0.8.21",
+};
+
+export default config;
