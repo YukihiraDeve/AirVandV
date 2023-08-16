3632238d788eba2459a8bc35ad043a04eb9a3432
Ronfflex <aiglevisnky@gmail.com>
2023-08-16 04:24:02 +0200

    upgradeable smartcontract. mint and other functions added

--- blockchain/contracts/AirVandV.sol
+++ blockchain/contracts/AirVandV.sol
@@ -1,31 +1,50 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract AirVandV is Initializable, ERC721Upgradeable, ERC721BurnableUpgradeable, OwnableUpgradeable {
+contract AirVandV is Initializable, ERC721EnumerableUpgradeable, OwnableUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
-
     CountersUpgradeable.Counter private _tokenIdCounter;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
+    // Mapping from token ID to door ID
+    mapping(uint256 => string) private _doorIds;
 
+    // Initialise le nom et le symbole du token et définit le propriétaire du contrat
     function initialize() initializer public {
         __ERC721_init("AirVandV", "VNV");
-        __ERC721Burnable_init();
         __Ownable_init();
     }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+    // Crée un nouveau token et l'attribue à l'adresse du destinataire (seule le propriétaire du contrat peut appeler cette fonction)
+    function mint(address to, string memory doorId) public onlyOwner {
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        uint256 newTokenId = _tokenIdCounter.current();
+        _safeMint(to, newTokenId);
+        _doorIds[newTokenId] = doorId;
+    }
+
+    // Renvoie l'identifiant de la porte associé à un ID de token spécifique
+    function getDoorId(uint256 tokenId) public view returns (string memory) {
+        return _doorIds[tokenId];
+    }
+
+    // Vérifie si l'adresse qui appelle la fonction est autorisé à déverrouiller une porte spécifique
+    function unlockDoor(string memory doorId) public view returns (bool) {
+        uint256 tokenId = getTokenIdForDoor(doorId);
+        return _isApprovedOrOwner(_msgSender(), tokenId);
+    }
+
+    // Renvoie l'ID de token associé à un identifiant de porte spécifique
+    function getTokenIdForDoor(string memory doorId) public view returns (uint256) {
+        for (uint256 i = 1; i <= _tokenIdCounter.current(); i++) {
+            if (keccak256(bytes(_doorIds[i])) == keccak256(bytes(doorId))) {
+                return i;
+            }
+        }
+        revert("Door ID does not exist");
     }
 }
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -2,11 +2,12 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const AirVandV = await ethers.getContractFactory("AirVandV");
-
+  console.log("Deploying AirVandV...");
+  
   // Deploy the contract using the OpenZeppelin upgrades plugin to ensure it's upgradeable
   const airVandV = await upgrades.deployProxy(AirVandV, [], { initializer: 'initialize' });
-
-  console.log("AirVandV deployed to:", airVandV.target);
+  await airVandV.deployed();
+  console.log("AirVandV deployed to:", airVandV.address);
 }
 
 main()
