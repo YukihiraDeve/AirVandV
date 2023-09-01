b0626fa22da3006e33ab873b0c1388a032b5b17c
Ronfflex <aiglevisnky@gmail.com>
2023-09-01 22:06:46 +0200

    .env

--- frontend/src/components/NukiComponent.tsx
+++ frontend/src/components/NukiComponent.tsx
@@ -1,62 +1,33 @@
 import React from "react";
 
-var SMARTLOCK_ID = "18118914148"
+const SMARTLOCK_ID = process.env.REACT_APP_SMARTLOCK_ID; //37F8B064
 const ENDPOINT = "https://api.nuki.io/smartlock/";
-const API_KEY = "f6466c0646699cce05e0c920cf1824d7d6271c2c9edef97ea328aa1cb4d0c6b3f0c40644fc8ff876" //process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-
-function hexToDec(SMARTLOCK_ID: string): number {
-  return parseInt(SMARTLOCK_ID, 16);
-}
-
-
-const decValue = hexToDec(SMARTLOCK_ID);
-console.log(`Calculated ID (DEC): ${decValue}`);
-
-
-//https://api.nuki.io/oauth/authorize?response_type=code&client_id=CLIENT_ID&redirect_uri=CALLBACK_URL&scope=SCOPES
-
+// const decValue = hexToDec(SMARTLOCK_ID as string);
+// console.log(`Calculated ID (DEC): ${decValue}`);
 
 function App() {
   const handleOpen = async () => {
     console.log("Opening the lock...");
     try {
-      console.log("start")
+      console.log("start");
       const response = await fetch(`${ENDPOINT}${SMARTLOCK_ID}/action/unlock`, {
         method: "POST",
         headers: {
-          'authorization': `Bearer ${API_KEY}`
+          authorization: `Bearer ${API_KEY}`,
         },
       });
-      console.log("before data") 
+      console.log("before data");
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      
     } catch (error) {
       console.error("Error opening the lock:", error);
     }
-     console.log("Finaly");
+    console.log("Done");
   };
-
-  const handleTest = async () => {
-    console.log("Testing the lock...");
-    try {
-      const response = await fetch(`${ENDPOINT}${SMARTLOCK_ID}/action/unlock`, {
-        method: "POST",
-        headers: {
-          authorization: `${API_KEY}`,
-        },
-      });
-
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Error testing the lock:", error);
-    }
-  };
-
 
   const handleClose = async () => {
     console.log("Closing the lock...");
@@ -64,14 +35,15 @@ function App() {
       const response = await fetch(`${ENDPOINT}${SMARTLOCK_ID}/action/lock`, {
         method: "POST",
         headers: {
-          authorization: `${API_KEY}`,
+          authorization: `Bearer ${API_KEY}`,
         },
       });
 
-      const data = await response.json();
-      console.log(data);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
     } catch (error) {
-      console.error("Error closing the lock:", error);
+      console.error("Error opening the lock:", error);
     }
   };
 
@@ -81,7 +53,6 @@ function App() {
         <h1>Nuki Smart Lock Controller</h1>
         <button onClick={handleOpen}>Open</button>
         <button onClick={handleClose}>Close</button>
-        <button onClick={handleTest}>Test</button>
       </header>
     </div>
   );
